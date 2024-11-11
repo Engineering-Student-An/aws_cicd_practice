5e010720278abe297f7e72b61b04eeb63e0db769
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-11-11 17:58:00 +0900

    feat: CI/CD 파이프라인 검증을 위한 코드 변경

--- src/main/java/com/example/elice/TestController.java
+++ src/main/java/com/example/elice/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
     @GetMapping
     public String home() {
-    return "Hello Elicexx";
+    return "Hello Elicexxsss";
     }
 
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
